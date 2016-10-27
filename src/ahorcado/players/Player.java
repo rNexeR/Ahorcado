d1d94d9914e107bd9519134067335538c3fd69a7
kenystev <kjestevez.0795@gmail.com>
2016-10-26 19:12:45 -0600

    current unimplemented functions in player and player have done

--- src/ahorcado/players/Player.java
+++ src/ahorcado/players/Player.java
@@ -1,18 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ahorcado.players;
 
-/**
- *
- * @author rnexer
- */
 public class Player {
 
+    private final String name;
+    private int complitedWords;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
     public void incrementScore(boolean completed){
-        
+        if(completed)
+            complitedWords++;
+    }
+
+    void init() {
+        complitedWords=0;
     }
     
 }
